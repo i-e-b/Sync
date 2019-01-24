d6810fe9f2b69c931d54a7819c2e34e820d20502
Iain Ballard <iain.ballard@gocompare.com>
2019-01-24 14:23:05 +0000

    Basic

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Sync
-A reasonable and reliable way to terminate C# async tasks
+
+A reasonable and reliable way to terminate C# async tasks.
+
+## Use
+
+```csharp
+// Async method that returns 'Task':
+Sync.Run(() => MyThing.DoSomethingAsync(...));
+
+// Async method that returns 'Task<T>':
+var result = Sync.Run(() => MyThing.ReadSomethingAsync(...));
+```
---
+++ Sync/SyncStandard/Sync.cs
@@ -0,0 +1,43 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Threading.Tasks
+{
+    /// <summary>
+    /// Helper class to properly wait for async tasks
+    /// </summary>
+    public static class Sync  
+    {
+        private static readonly TaskFactory _taskFactory = new
+            TaskFactory(CancellationToken.None,
+                TaskCreationOptions.None,
+                TaskContinuationOptions.None,
+                TaskScheduler.Default);
+
+        /// <summary>
+        /// Run an async function synchronously and return the result
+        /// </summary>
+        public static TResult Run<TResult>(Func<Task<TResult>> func)
+        {
+            if (_taskFactory == null) throw new Exception("Static init failed");
+            if (func == null) throw new ArgumentNullException(nameof(func));
+
+            var rawTask = _taskFactory.StartNew(func).Unwrap();
+            if (rawTask == null) throw new Exception("Invalid task");
+
+            return rawTask.GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Run an async function synchronously
+        /// </summary>
+        public static void Run(Func<Task> func)
+        {
+            if (_taskFactory == null) throw new Exception("Static init failed");
+            if (func == null) throw new ArgumentNullException(nameof(func));
+
+            var rawTask = _taskFactory.StartNew(func).Unwrap();
+            if (rawTask == null) throw new Exception("Invalid task");
+
+            rawTask.GetAwaiter().GetResult();
+        }
+    }
+}
